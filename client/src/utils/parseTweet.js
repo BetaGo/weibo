a12fe9abaa2d57d35e5a4373a2d7ccbe531760fc
AlphaGo <gouwantong@live.com>
2017-09-18 18:26:08 +0800

    add parse RegExp

--- client/src/utils/parseTweet.js
+++ client/src/utils/parseTweet.js
@@ -1,4 +1,6 @@
 // @flow
-function parseTweet(tweet: string) {
-  const re = /https?:\/\/[^\s]*\s/i;
-}
+
+const urlRe = /\s(https?:\/\/[^\s]*)\s/g;
+const atRe = /\s@([^\s]*)\s/g;
+
+function parseTweet(tweet: string) {}
